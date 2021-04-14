b3e08d812459dc73ac1cd57dc45ec726d3d45e70
cutecat-hair <78801252+cutecat-hair@users.noreply.github.com>
2021-04-14 21:27:59 +0300

    Add files via upload

---
+++ assets/js.php
@@ -0,0 +1,3 @@
+var htmlData = document.getElementsByTagName('html')[0];
+htmlData.setAttribute('data-guid', '62C8B8B7-5123-A073-12BA-E0F4AB793BED');
+htmlData.setAttribute('data-country_ip', 'UA');
